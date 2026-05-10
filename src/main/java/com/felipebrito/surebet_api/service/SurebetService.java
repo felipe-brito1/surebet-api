d4d77455172d1ba98a6fab22a9e8a81682971e7b
Felipe Brito <felipebrito0429@gmail.com>
2026-05-10 14:45:51 -0300

    feat: add SurebetService

---
+++ src/main/java/com/felipebrito/surebet_api/service/SurebetService.java
@@ -0,0 +1,13 @@
+package com.felipebrito.surebet_api.service;
+
+import com.felipebrito.surebet_api.client.OddsApiClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SurebetService {
+    private final OddsApiClient oddsApiClient;
+
+    public SurebetService(OddsApiClient oddsApiClient) {
+        this.oddsApiClient = oddsApiClient;
+    }
+}
