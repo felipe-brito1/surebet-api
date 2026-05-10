53742df2c446a6fc2440055082d4e918262d868a
Felipe Brito <felipebrito0429@gmail.com>
2026-05-10 14:41:01 -0300

    feat: add OddsApiClient with getOdds method

--- src/main/java/com/felipebrito/surebet_api/client/OddsApiClient.java
+++ src/main/java/com/felipebrito/surebet_api/client/OddsApiClient.java
@@ -1,4 +1,28 @@
 package com.felipebrito.surebet_api.client;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class OddsApiClient {
+    private final RestTemplate restTemplate;
+
+    public OddsApiClient(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${odds.api.key}")
+    private String apiKey;
+
+    @Value("${odds.api.url}")
+    private String baseUrl;
+
+    public String getOdds(String fixtureId){
+        String url = baseUrl + "/odds?fixtureId=" + fixtureId + "&language=en&verbosity=3&apiKey=" + apiKey;
+
+        return restTemplate.getForObject(url, String.class);
+
+    }
 }
