49b6652d78140034b35a7d7deae61c4c3be620e7
Felipe Brito <felipebrito0429@gmail.com>
2026-05-10 14:23:47 -0300

    first commit

---
+++ src/main/java/com/felipebrito/surebet_api/SurebetApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipebrito.surebet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurebetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurebetApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/felipebrito/surebet_api/client/OddsApiClient.java
@@ -0,0 +1,4 @@
+package com.felipebrito.surebet_api.client;
+
+public class OddsApiClient {
+}
